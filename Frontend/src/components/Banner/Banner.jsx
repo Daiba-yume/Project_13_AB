ab7c24d56b77777f8ca3fd27fe8c76adb0a71832
daiba <daiba_b@yahoo.com>
2025-02-20 16:58:16 +0100

    Mise en place des composants Banner, Card et add pictures

---
+++ Frontend/src/components/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+import "./Banner.scss";
+const Banner = () => {
+  return (
+    <div className="Banner">
+      <section className="bannerSection">
+        <h2 className="sr-only">Promoted Content</h2>
+        <p className="subtitle">No fees.</p>
+        <p className="subtitle">No minimum deposit.</p>
+        <p className="subtitle">High interest rates.</p>
+        <p className="text">Open a savings account with Argent Bank today!</p>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;
