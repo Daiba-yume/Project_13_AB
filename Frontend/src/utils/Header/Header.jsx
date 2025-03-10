56a047b76886a7dec4755cbd4933d26db186c365
daiba <daiba_b@yahoo.com>
2025-03-10 15:30:57 +0100

    Correction du style et mise à jour du header avec le logout

--- Frontend/src/utils/Header/Header.jsx
+++ Frontend/src/utils/Header/Header.jsx
@@ -3,26 +3,34 @@ import Logo from "../../assets/argentBankLogo.png";
 import { FaUserCircle } from "react-icons/fa";
 import { FaRightFromBracket } from "react-icons/fa6";
 import "./Header.scss";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const token = useSelector((state) => state.auth.token);
+  const firstName = useSelector((state) => state.auth.userInfos?.firstName);
   return (
     <nav className="header">
       <NavLink className="headerLogo" to="/">
         <img className="logo" src={Logo} alt="logo argentBank" />
       </NavLink>
-      <div>
+
+      {!token ? (
         <NavLink className="logIn" to="/Login">
           <FaUserCircle className="signInIcon" />
           Sign In
         </NavLink>
-      </div>
-      {/* logout à faire */}
-      <div className="logout">
-        <NavLink className="signOut">
-          <FaRightFromBracket />
-          Sign Out
-        </NavLink>
-      </div>
+      ) : (
+        <div className="logOut">
+          <NavLink className="userInfo" to="/profile">
+            <FaUserCircle className="signOutIcon" />
+            {firstName}
+          </NavLink>
+          <NavLink className="signOut" to="/login">
+            <FaRightFromBracket />
+            Sign Out
+          </NavLink>
+        </div>
+      )}
     </nav>
   );
 };
