5dd0999c0715b0f9b1a794dd26b9177ba1536f9b
daiba <daiba_b@yahoo.com>
2025-03-25 21:32:57 +0100

    Fix userInfos null + add comments

--- Frontend/src/components/ProfileHeader/ProfileHeader.jsx
+++ Frontend/src/components/ProfileHeader/ProfileHeader.jsx
@@ -5,10 +5,12 @@ import EditName from "../EditName/EditName";
 
 const ProfileHeader = () => {
   const [isEditing, setIsEditing] = useState(false);
-  //On récupère les infos user dpuis Redux
-  const { firstName, lastName } = useSelector(
-    (state) => state.auth.userInfos || {} // si aucune data retourne un obj vide
-  );
+  //On récupère les infos user dpuis le store Redux
+  const userInfos = useSelector((state) => state.auth.userInfos);
+  // Sécurisation des valeurs pour éviter les erreurs si userInfos est null
+  const firstName = userInfos?.firstName || "";
+  const lastName = userInfos?.lastName || "";
+  // Fonction appelée au clic pour "Edit Name"
   const handleEditClick = () => {
     setIsEditing(true);
   };
